f66cde914bbbc4c0f1fa89b1e64c284d251253f2
ajaysunnydk <ajaysunnydk167@gmail.com>
2022-09-20 00:31:02 +0530

    OddEven

---
+++ firstProject/oddEven.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstProject
+{
+    internal class oddEven
+    {
+        static void main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter any Number: ");
+            num = Console.Read();
+
+            Console.WriteLine("Number is "+num);
+            if (num%2 == 0)
+            {
+                Console.WriteLine(num+" is Even");
+            }
+            else
+            {
+                Console.WriteLine(num+" is Odd");
+            }
+        }
+    }
+}
