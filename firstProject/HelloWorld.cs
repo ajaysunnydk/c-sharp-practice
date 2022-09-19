c5d9c140bd55f22f41306530011265897f0155aa
ajaysunnydk <ajaysunnydk167@gmail.com>
2022-09-20 00:21:41 +0530

    Add project files.

---
+++ firstProject/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace firstProject
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ajay Sunny";
+            Console.WriteLine("Hello World "+name);
+        }
+    }
+}
